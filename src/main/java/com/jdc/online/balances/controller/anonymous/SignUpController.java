4cefaff82dad0dad43f77a56c7d310ebf6c8a1a0
Phone Pyae Naing <murasakinaing@gmail.com>
2025-02-24 23:41:00 +0630

    Basics Project Structure

---
+++ src/main/java/com/jdc/online/balances/controller/anonymous/SignUpController.java
@@ -0,0 +1,16 @@
+package com.jdc.online.balances.controller.anonymous;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/signup")
+public class SignUpController {
+
+	@GetMapping
+	String index() {
+		return "anonymouse/signup";
+	}
+	
+}
